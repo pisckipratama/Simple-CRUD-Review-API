ca0169171112954c66cf5124a2af96074b928069
Piscki Pratama <pisckipratama@gmail.com>
2020-02-29 13:29:15 +0700

    make it better

--- app.js
+++ app.js
@@ -17,18 +17,26 @@ const app = express();
 app.use(cors());
 app.use(logger('dev'));
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
 app.use(bodyParser.json());
 
 // database config
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/review', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: true
-})
-.then(() => console.log('successfully connected with mongodb.'))
-.catch(err => console.error(err))
+const db = process.env.MONGODB_URL;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+    });
+    console.log("MongoDB is Connected...");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
 
 app.use('/', indexRouter);
 
